d146b5d8f86471da65d5e9a77772aaaf69e26b26
javlonartikbaev <artjavlon@gmail.com>
2024-06-01 01:18:37 +0500

    html changed all files

--- MediCio/settings.py
+++ MediCio/settings.py
@@ -109,7 +109,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '/static/'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
